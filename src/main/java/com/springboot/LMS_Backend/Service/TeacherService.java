de1879dfa200603b31d1b68ea35748519af6833e
Avishka Rodrigo <avish.rodrigo11@gmail.com>
2022-10-15 18:02:38 +0530

    added Teacher controllers(CRUD)

--- src/main/java/com/springboot/LMS_Backend/Service/TeacherService.java
+++ src/main/java/com/springboot/LMS_Backend/Service/TeacherService.java
@@ -1,6 +1,7 @@
 package com.springboot.LMS_Backend.Service;
 
 import com.springboot.LMS_Backend.model.Teacher;
+import com.springboot.LMS_Backend.repository.TeacherRespository;
 
 import java.util.List;
 
@@ -8,5 +9,7 @@ public interface TeacherService {
 
     public Teacher saveTeacher(Teacher teacher);
     public List<Teacher> getAllteachers();
+    public Teacher getTeacherById (Integer id);
 
+    public void deleteTeacher(Integer id);
 }
