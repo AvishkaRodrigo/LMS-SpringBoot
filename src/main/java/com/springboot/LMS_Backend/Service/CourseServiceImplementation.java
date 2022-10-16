32912f4fdf27ad3a0db77fbcfa2396fd2dbf02e0
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2022-10-16 14:58:36 +0530

    fix: course service

--- src/main/java/com/springboot/LMS_Backend/Service/CourseServiceImplementation.java
+++ src/main/java/com/springboot/LMS_Backend/Service/CourseServiceImplementation.java
@@ -1,12 +1,12 @@
 package com.springboot.LMS_Backend.Service;
 
 import com.springboot.LMS_Backend.model.Course;
-import com.springboot.LMS_Backend.model.Student;
 import com.springboot.LMS_Backend.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseServiceImplementation implements CourseService{
@@ -15,6 +15,11 @@ public class CourseServiceImplementation implements CourseService{
     @Override
     public Course saveCourse(Course course) {
         return repository.save(course);
+    }
+
+    @Override
+    public Optional<Course> getSpecificCourse(int id) {
+        return repository.findById(id);
     }
 
     @Override
@@ -29,7 +34,13 @@ public class CourseServiceImplementation implements CourseService{
     }
 
     @Override
-    public List<Course> getAllCoursesOfStudent(Student student) {
-        return repository.findAll();
+    public List<Course> getAllCoursesOfStudent(int id) {
+
+        return repository.getStudentCourses(id);
+    }
+
+    @Override
+    public List<Course> getAvailableCourses(int id) {
+        return repository.getNotEnrolledCourses(id);
     }
 }
